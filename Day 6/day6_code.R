a77853f49dd19f1923d1f56de528f2a52ad39549
Oscar Oelrich <oscar.oelrich@live.com>
2020-12-06 20:59:01 +0100

    n

--- Day 6/day6_code.R
+++ Day 6/day6_code.R
@@ -4,7 +4,8 @@ library(stringr)
 
 data <- readChar("Day 6/day6_data.txt", file.info("Day 6/day6_data.txt")$size)
 data <- str_split(data, "\n\n")
-for (i in seq_len(473)) {
+n <- length(data[[1]])
+for (i in seq_len(n)) {
     data[[1]][i] <- str_remove_all(data[[1]][i], "\n")
 }
 
@@ -12,7 +13,7 @@ for (i in seq_len(473)) {
 # Problem 1 ###################################################################
 
 affirmative <- c()
-for (i in seq_len(473)) {
+for (i in seq_len(n)) {
     affirmative[i] <- sum(!!str_count(data[[1]][i], letters))
 }
 sum(affirmative)
@@ -24,8 +25,8 @@ sum(affirmative)
 data <- readChar("Day 6/day6_data.txt", file.info("Day 6/day6_data.txt")$size)
 data <- str_split(data, "\n\n")
 
-in_all_val <- rep(0, 473)
-for (i in seq_len(473)) {
+in_all_val <- rep(0, n)
+for (i in seq_len(n)) {
     data2 <- str_split(data[[1]][i], "\n")
     in_all <- rep(1, 26)
     for (j in seq_len(length(data2[[1]]))) {
