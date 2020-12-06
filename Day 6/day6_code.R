2a760722c340932e33a570b6814ade00cb1dc959
Oscar Oelrich <oscar.oelrich@live.com>
2020-12-06 20:56:23 +0100

    fever edition

--- Day 6/day6_code.R
+++ Day 6/day6_code.R
@@ -1,3 +1,36 @@
-# Data-wrangling ##########################################
+# Data-wrangling ##############################################################
 
-data <- read.csv("Day 6/day6_data.txt", header = F)
+library(stringr)
+
+data <- readChar("Day 6/day6_data.txt", file.info("Day 6/day6_data.txt")$size)
+data <- str_split(data, "\n\n")
+for (i in seq_len(473)) {
+    data[[1]][i] <- str_remove_all(data[[1]][i], "\n")
+}
+
+
+# Problem 1 ###################################################################
+
+affirmative <- c()
+for (i in seq_len(473)) {
+    affirmative[i] <- sum(!!str_count(data[[1]][i], letters))
+}
+sum(affirmative)
+!!str_count(data[[1]][1], letters)
+
+
+# Problem 2 ###################################################################
+
+data <- readChar("Day 6/day6_data.txt", file.info("Day 6/day6_data.txt")$size)
+data <- str_split(data, "\n\n")
+
+in_all_val <- rep(0, 473)
+for (i in seq_len(473)) {
+    data2 <- str_split(data[[1]][i], "\n")
+    in_all <- rep(1, 26)
+    for (j in seq_len(length(data2[[1]]))) {
+        in_all <- in_all * as.numeric(!!str_count(data2[[1]][j], letters))
+    }
+    in_all_val[i] <- sum(in_all)
+}
+sum(in_all_val)
